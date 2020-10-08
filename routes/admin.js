76afdcacc27720028a58d8a6d72b407b90ec5b2b
Bruno Estevam <67297226+BrunoEstevamRJ@users.noreply.github.com>
2020-10-08 18:19:54 -0300

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("Página principal do painel ADM");
+  res.render("admin/index");
 });
 
 router.get("/posts", (req, res) => {
@@ -10,11 +10,15 @@ router.get("/posts", (req, res) => {
 });
 
 router.get("/categorias", (req, res) => {
-  res.send("Página de categorias");
+  res.render("admin/categorias");
+});
+
+router.get("/categorias/add", (req, res) => {
+  res.render("admin/addcategorias");
 });
 
 router.get("/teste", (req, res) => {
-  res.send("Isso é um teste")
-})
+  res.send("Isso é um teste");
+});
 
 module.exports = router;
